88b584406f157a586c4566472bb66d0115204302
Jason Mahirwe <bjthe2nd@gmail.com>
2021-07-10 17:15:12 +0200

    Implement validating function for args passed to CreateByAdmin func

--- validators/UserValidators.go
+++ validators/UserValidators.go
@@ -12,11 +12,11 @@ func ValidateCreateByAdminArgs(args models.CreateByAdminArgs) error {
 	err := validation.ValidateStruct(&args,
 		// Name cannot be empty
 		validation.Field(&args.Name, validation.Required.Error("name is required")),
-		// Email cannot be empty, and must be a string consisting of two letters in upper case
+		// Email cannot be empty, and must be a valid email
 		validation.Field(&args.Email, validation.Required.Error("email is required"), is.Email.Error("Invalid email")),
 		// Title cannot be empty
 		validation.Field(&args.Title, validation.Required.Error("title is required")),
-		// State cannot be empty, and must be a string consisting of five digits
+		// Birthdate cannot be empty, and must be a date string of the format "YYYY-MM-DD"
 		validation.Field(&args.Birthdate, validation.Required.Error("birthdate is required"), validation.Date("2006-01-02").Error("Invalid date for birthdate. Format: YYYY-MM-DD")),
 	)
 
