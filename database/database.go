de9a7f9a49efd0d3f3b6b7dbbb9c33058ed5f708
Jason Mahirwe <bjthe2nd@gmail.com>
2021-07-13 12:00:22 +0200

    Create an index on the email field

--- database/database.go
+++ database/database.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"server/config"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ConnectDB(ctx context.Context, conf config.MongoConfiguration) *mongo.Database {
+func connectDB(ctx context.Context, conf config.MongoConfiguration) *mongo.Database {
 	connection := options.Client().ApplyURI(conf.Server)
 
 	client, err := mongo.Connect(ctx, connection)
@@ -19,4 +20,41 @@ func ConnectDB(ctx context.Context, conf config.MongoConfiguration) *mongo.Datab
 
 	fmt.Println("Database connected!")
 	return client.Database(conf.Database)
+}
+
+func SetupDatabaseClient() *UsersClient {
+	conf := config.GetConfig()
+	ctx := context.TODO()
+
+	db := connectDB(ctx, conf.Mongo)
+	collection := db.Collection(conf.Mongo.Collection)
+
+	client := &UsersClient{
+		Col: collection,
+		Ctx: ctx,
+	}
+
+	err := createIndices(client)
+	if err != nil {
+		panic(err)
+	}
+
+	return client
+}
+
+func createIndices(usersClient *UsersClient) error {
+
+	// Create an index model for the field: email
+	mod := mongo.IndexModel{
+		Keys:    bson.M{"email": 1},
+		Options: options.Index().SetUnique(true),
+	}
+
+	// Create the above index on the users collection
+	_, err := usersClient.Col.Indexes().CreateOne(usersClient.Ctx, mod)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,44 +1,6 @@
 package main
 
-// import (
-// 	"log"
-// 	"os"
-// 	"server/config"
-// 	"server/routes"
-// 	"github.com/gofiber/fiber/v2"
-// 	"github.com/gofiber/fiber/v2/middleware/cors"
-// 	"github.com/gofiber/fiber/v2/middleware/logger"
-// 	"github.com/joho/godotenv"
-// )
-
-// func main() {
-// 	if os.Getenv("APP_ENV") != "production" {
-// 		err := godotenv.Load()
-// 		if err != nil {
-// 			log.Fatal("Error loading .env file")
-// 		}
-// 	}
-
-// 	app := fiber.New()
-
-// 	app.Use(cors.New())
-// 	app.Use(logger.New())
-
-// 	config.ConnectDB()
-
-// 	setupRoutes(app)
-
-// 	port := os.Getenv("PORT")
-// 	err := app.Listen(":" + port)
-
-// 	if err != nil {
-// 		log.Fatal("Error app failed to start")
-// 		panic(err)
-// 	}
-// }
-
 import (
-	"context"
 	"log"
 	"server/config"
 	"server/database"
@@ -61,16 +23,7 @@ func setupRoutes(app *fiber.App) {
 }
 
 func main() {
-	conf := config.GetConfig()
-	ctx := context.TODO()
-
-	db := database.ConnectDB(ctx, conf.Mongo)
-	collection := db.Collection(conf.Mongo.Collection)
-
-	client := &database.UsersClient{
-		Col: collection,
-		Ctx: ctx,
-	}
+	client := database.SetupDatabaseClient()
 
 	app := fiber.New()
 
